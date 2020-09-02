5967afdf691c7a11f1f025efbd6658183e102f4d
t-nakamura-itsol <t-nakamura-itsol@users.noreply.github.com>
2020-09-02 15:51:18 +0000

    deploy: 84f626df8e523d4536fe02d89b93f209cc0984eb

---
+++ precache-manifest.f85d3fd17f10a844340c3620628f5ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c504e4dc602c4b24595a2fa8ce466a7f",
+    "url": "/deploy_react/index.html"
+  },
+  {
+    "revision": "a5196dee33f86c1f17af",
+    "url": "/deploy_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34610b2aef68be945cd4",
+    "url": "/deploy_react/static/js/2.18ca8854.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy_react/static/js/2.18ca8854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5196dee33f86c1f17af",
+    "url": "/deploy_react/static/js/main.6aeff502.chunk.js"
+  },
+  {
+    "revision": "1d10346317be603bd478",
+    "url": "/deploy_react/static/js/runtime-main.a2fb37c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/t-nakamura-itsol/deploy_react/precache-manifest.90d97b711fb7092faf0c007fc8fcd553.js"
+  "/deploy_react/precache-manifest.f85d3fd17f10a844340c3620628f5ee6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/t-nakamura-itsol/deploy_react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
