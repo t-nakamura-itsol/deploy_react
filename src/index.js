fb2cff37f645ac88384a0597d0004ba607f9a074
t-nakamura-itsol <t-nakamura@itsol.co.jp>
2020-09-15 20:04:59 +0900

    comment out

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Welcome from './Welcome';
+// import App from './App';
+// import Welcome from './Welcome';
 import Clock from './Clock';
 import * as serviceWorker from './serviceWorker';
 
