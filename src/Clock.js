580ec1c1b18b281cb2ee4073d42b647d6bde1140
t-nakamura-itsol <t-nakamura@itsol.co.jp>
2020-09-29 21:05:38 +0900

    modify h1

--- src/Clock.js
+++ src/Clock.js
@@ -26,7 +26,7 @@ export class Clock extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello, world!</h1>
+                <h1>Hello, world! World!World!World!World!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
